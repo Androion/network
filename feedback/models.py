d4e11f4c0dd6f73a704bf921149b0984487ec27e
androion <androion@androion.com>
2018-11-05 20:35:57 +0900

    Initial commit

---
+++ feedback/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import User
+
+class Subject(models.Model):
+    subject = models.CharField(max_length=20, verbose_name='과목')
+    created_date = models.DateTimeField(auto_now=True)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return str(self.subject)
+
+class Post(models.Model):
+    subject = models.ForeignKey('feedback.Subject', default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    goodPoint = models.TextField(verbose_name='좋았던 점', default=' ')
+    improvingPoint = models.TextField(verbose_name='개선되었으면 하는 점', default=' ')
+    anotherPoint = models.TextField(verbose_name='하고싶은 말', default=' ')
+    created_date = models.DateTimeField(auto_now=True)
+    numOfLike = models.IntegerField(default=0)
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.user)
+
+class Comment(models.Model):
+	post = models.ForeignKey('feedback.Post', related_name='comments', default=1, on_delete=models.CASCADE)
+	commentDesc = models.TextField(verbose_name='댓글', default=' ')
+	created_date = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return str(self.commentDesc)
+
+class Like(models.Model):
+    post = models.ForeignKey('feedback.Subject', default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE,)
+    isUserLiked = models.DateTimeField(auto_now_add=True)
